1b4c1f69341dca35c24d984b7a83c1e7df612fd6
Resurrection <30309647+Resurrection3387@users.noreply.github.com>
2020-03-28 11:21:26 +0200

    e53g: PixelExperience bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := device/samsung/e53g
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_e53g.mk \
-	$(LOCAL_DIR)/pa_e53g.mk \
-	$(LOCAL_DIR)/lineage_e53g.mk
+	$(LOCAL_DIR)/aosp_e53g.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_e53g-eng \
+    aosp_e53g-userdebug \
+    aosp_e53g-user
