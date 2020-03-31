4f0ee0b019eef887ee131ce7952c360a47f75528
ma3a_xata <v.radulevich@gmail.com>
2020-03-31 16:57:20 +0300

    e53g: fix compilation

--- aosp_e53g.mk
+++ aosp_e53g.mk
@@ -1,10 +1,10 @@
 # Inherit from common
 $(call inherit-product, device/samsung/msm8916-common/aosp.mk)
 
-$(call inherit-product, device/samsung/j53gxx/device.mk)
+$(call inherit-product, device/samsung/e53g/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := j53gxx
-PRODUCT_NAME := aosp_j53gxx
-PRODUCT_MODEL := SM-J500H
+PRODUCT_DEVICE := e53g
+PRODUCT_NAME := aosp_e53g
+PRODUCT_MODEL := SM-E500H
 
