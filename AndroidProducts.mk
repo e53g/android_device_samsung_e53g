3d70ec84ef348428e7b1830be1d2e20c9e5eedaf
ma3axata <v.radulevich@gmail.com>
2021-02-25 21:58:44 +0200

    e53g: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,3 +3,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_e53g.mk \
 	$(LOCAL_DIR)/pa_e53g.mk \
 	$(LOCAL_DIR)/lineage_e53g.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_e53g-eng \
+    lineage_e53g-userdebug \
+    lineage_e53g-user
