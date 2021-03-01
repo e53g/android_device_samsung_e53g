a37c5cf88441fa1a7f02363c7c53464670f0903e
ma3axata <v.radulevich@gmail.com>
2021-03-01 13:14:56 +0200

    e53g: add config_enabled_lte=false
    E500H/DS dosn't support lte(fix ril)
    
    * remove unsupported config_radio_access_family
    * clenup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@ include device/samsung/e5-common/BoardConfigCommon.mk
 LOCAL_PATH := device/samsung/e53g
 
 # Asserts
-TARGET_OTA_ASSERT_DEVICE := e53g
+TARGET_OTA_ASSERT_DEVICE := e53g,e53gxx
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_e53g_eur_defconfig
@@ -15,4 +15,3 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE  := 12444667740
 
 # RIL
 SIM_COUNT := 2
-BOARD_GLOBAL_CFLAGS += -DRIL_FIX_SMS_NOT_SENT_ERR
